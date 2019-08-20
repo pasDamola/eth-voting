0cd057cb0b98d4d8a51c804cc32c9f451d2bdb40
oyincode <oyindadavid18@gmail.com>
2019-08-20 12:15:29 +0100

    authorize function

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.0 <0.7.0;
 
 contract Election{
-     address public owner;
+    address public owner;
     string public nameoFElection;
     
     event Vote(string candidateName, uint voteCount);
@@ -15,5 +15,32 @@ contract Election{
         bool authorized;
         bool voted;
         uint vote; // candidate id
+    }
+    
+    uint public totalVotes;
+    Candidate[] public candidates;
+    mapping(address => Voter) public voters;
+    
+    modifier ownerOnly(){
+        require(owner == msg.sender, "Only owner of contract is allowed to do this");
+        _;
+    }
+    
+    constructor(string memory _name) public {
+        owner = msg.sender;
+        nameoFElection = _name;
+    }
+    
+    function getNumCandidates() public view returns(uint){
+        return candidates.length;
+    }
+    
+    function addCandidates(string memory _name) public ownerOnly {
+        candidates.push(Candidate(_name, 0));
+    }
+    
+    function authorize(address _voter) public ownerOnly{
+        require(voters[_voter].voted, "Voter has already voted");
+        voters[_voter].authorized = true;
     }
 }
