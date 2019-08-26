6c4297cbba4d9c597900ac0f78f713c4df34704e
oyincode <oyindadavid18@gmail.com>
2019-08-26 13:50:53 +0100

    nams of two candidates must be set

--- test/Election.test.js
+++ test/Election.test.js
@@ -1,15 +1,17 @@
 const Election = artifacts.require("./Election.sol");
 
 contract("Election", accounts => {
-  it("name and two candidates should be set in the constructor", async () => {
+  it("name should be set in the constructor", async () => {
     this.election = await Election.deployed();
     this.name = await this.election.nameoFElection.call();
-    this.candidate1 = await this.election.candidates.call(0);
-    this.candidate2 = await this.election.candidates.call(1);
-    assert.equal(this.name, "Presidential Elections", "name was not expected");
+    assert.equal(this.name, "Presidential Elections", "name was expected");
   });
 
-  // it("deploys successfully", async () => {
-  //   const instance = await Election();
-  // });
+  it("two candidates should be set in the constructor", async () => {
+    this.election = await Election.deployed();
+    this.candidate1 = await this.election.candidates.call(0);
+    this.candidate2 = await this.election.candidates.call(1);
+    assert.equal(this.candidate1[0], "Atiku", "it should be atiku");
+    assert.equal(this.candidate2[0], "Buhari", "it must be buhari");
+  });
 });
