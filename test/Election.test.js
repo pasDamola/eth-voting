f871f577b05728282074575b412c4aaf02cbb895
oyincode <oyindadavid18@gmail.com>
2019-08-26 13:37:20 +0100

    name of election test

--- test/Election.test.js
+++ test/Election.test.js
@@ -1,11 +1,15 @@
-const Ballot = artifacts.require("./Ballot.sol");
+const Election = artifacts.require("./Election.sol");
 
-contract("Ballot", accounts => {
-  before(async () => {
-    this.ballot = await Ballot.deployed();
+contract("Election", accounts => {
+  it("name and two candidates should be set in the constructor", async () => {
+    this.election = await Election.deployed();
+    this.name = await this.election.nameoFElection.call();
+    this.candidate1 = await this.election.candidates.call(0);
+    this.candidate2 = await this.election.candidates.call(1);
+    assert.equal(this.name, "Presidential Elections", "name was not expected");
   });
 
-  it("deploys successfully", async () => {
-    const instance = await Ballot();
-  });
+  // it("deploys successfully", async () => {
+  //   const instance = await Election();
+  // });
 });
